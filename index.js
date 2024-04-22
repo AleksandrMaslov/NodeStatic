8698d21d3bdcb860d7097abf3e79a8e88964d688
AleksandrMaslov <amaslov8910@gmail.com>
2024-04-22 18:44:38 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+const express = require('express');
+const cors = require('cors');
+const compression = require('compression')
+
+const PORT = 5173;
+
+const app = express();
+app.use(cors())
+app.use(compression())
+
+app.use('/PSViewerDemo',express.static(path.join(__dirname, 'public')));
+app.use('/PSViewerDemo/*',express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res) {
+  res.status(400);
+  return res.send(`<h1>404 Error: Resource not found</h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`HTTP server is listening PORT: ${PORT}`);
+})
+
+// // HTTPS Server
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync('/etc/letsencrypt/live/my_api_url/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/my_api_url/fullchain.pem'),
+// }, app);
+
+// httpsServer.listen(PORT, () => {
+//     console.log(`HTTPS Server running on port ${PORT}`);
+// });
