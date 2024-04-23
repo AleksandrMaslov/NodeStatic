e6bbd8713c4d921e720b0a58f56f4cc32fc15572
AleksandrMaslov <amaslov8910@gmail.com>
2024-04-23 12:56:39 +0300

    add: public

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors())
 app.use(compression())
 
-app.use('/PSViewerDemo',express.static(path.join(__dirname, 'public')));
+app.use('/',express.static(path.join(__dirname, 'public')));
 // app.use('/PSViewerDemo/*',express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res) {
