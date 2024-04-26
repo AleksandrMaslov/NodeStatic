b54d5dc251ffaf46747deeadda719e6e4a8fbe1c
AleksandrMaslov <amaslov8910@gmail.com>
2024-04-26 18:36:48 +0300

    add: redirect

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ app.use(cors())
 app.use(compression())
 
 app.use('/',express.static(path.join(__dirname, 'public')));
-// app.use('/PSViewerDemo/*',express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res) {
+app.use('/*',express.static(path.join(__dirname, 'public')));
+
+// app.use('/**/*',express.static(path.join(__dirname, 'public')));
+
+// app.all('*', function(_req, res) { 
+//   res.redirect('/index.html'); 
+// });
+
+app.use(function(_req, res) {
   res.status(400);
   return res.send(`<h1>404 Error: Resource not found</h1>`);
 });
